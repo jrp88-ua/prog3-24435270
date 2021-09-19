1421827f73cee5e6727ad7ecd01672c29ef15a23
jrp88-ua <jrp88@alu.ua.es>
2021-09-19 16:16:19 +0200

    Add tests

--- prog3-ImperialCommander/src/model/Coordinate.java
+++ prog3-ImperialCommander/src/model/Coordinate.java
@@ -6,7 +6,7 @@ package model;
  * @author Javier Rodriguez Perez - 24435270R
  */
 public class Coordinate {
-	
+
 	/**
 	 * x component
 	 */
@@ -15,7 +15,7 @@ public class Coordinate {
 	 * y component
 	 */
 	private int y;
-	
+
 	/**
 	 * Creates a new coordinate with the given values
 	 * 
@@ -26,7 +26,7 @@ public class Coordinate {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	/**
 	 * Creates a copy of the given coordinate
 	 * 
@@ -35,69 +35,92 @@ public class Coordinate {
 	public Coordinate(Coordinate coordinate) {
 		this(coordinate.getX(), coordinate.getY());
 	}
-	
+
 	/**
 	 * Compares this coordinate with the given object
 	 * 
 	 * @param other The other object to compare
 	 * 
-	 * @return True if {@code this == other} or both components of the coordinates match, false otherwise 
+	 * @return True if {@code this == other} or both components of the coordinates
+	 *         match, false otherwise
 	 */
 	@Override
-	public boolean equals(Object other) {
-		if(this == other)
+	public boolean equals(Object obj) {
+		if (this == obj) {
 			return true;
-		if(other == null)
+		}
+		if (!(obj instanceof Coordinate)) {
 			return false;
-		if(!(other instanceof Coordinate))
+		}
+		Coordinate other = (Coordinate) obj;
+		if (x != other.x) {
 			return false;
-		Coordinate coordinate = (Coordinate) other;
-		return getX() == coordinate.getX() && getY() == coordinate.getY();
+		}
+		if (y != other.y) {
+			return false;
+		}
+		return true;
 	}
-	
+
 	/**
-	 * Creates a new coordinate with its components being the sum of the instance and the given coordinate
+	 * Creates the hash code of the coordinate
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+	/**
+	 * Creates a new coordinate with its components being the sum of the instance
+	 * and the given coordinate
 	 * 
 	 * @param c The other coordinate, cannot be null
 	 * 
-	 * @return A new coordinate with its components being the sum of both coordinates
+	 * @return A new coordinate with its components being the sum of both
+	 *         coordinates
 	 */
 	public Coordinate add(Coordinate c) {
 		return add(c.getX(), c.getY());
 	}
-	
+
 	/**
-	 * Creates a new coordinate with its components being the sum of the instance and the given coordinate
+	 * Creates a new coordinate with its components being the sum of the instance
+	 * and the given coordinate
 	 * 
 	 * @param x The x component
 	 * @param y The y component
 	 * 
-	 * @return A new coordinate with its components being the sum of both coordinates
+	 * @return A new coordinate with its components being the sum of both
+	 *         coordinates
 	 */
 	public Coordinate add(int x, int y) {
 		return new Coordinate(getX() + x, getY() + y);
 	}
-	
+
 	/**
 	 * @return The x component of the coordinate
 	 */
 	public int getX() {
 		return x;
 	}
-	
+
 	/**
 	 * @return The y component of the coordinate
 	 */
 	public int getY() {
 		return y;
 	}
-	
+
 	/**
 	 * @return A string representing the coordinate with the format {@code [x, y]}
 	 */
 	@Override
 	public String toString() {
-		return "[" + getX() + ", " + getY() + "]";
+		return "[" + getX() + "," + getY() + "]";
 	}
-	
+
 }
