a10ebec3c2bb08c61f4933fe82f305f3200b01c6
jrp88-ua <jrp88@alu.ua.es>
2021-09-19 15:37:27 +0200

    Add Coordinate class

--- prog3-ImperialCommander/src/model/Coordinate.java
+++ prog3-ImperialCommander/src/model/Coordinate.java
@@ -1,5 +1,103 @@
 package model;
 
+/**
+ * Immutable class that holds an x and y value
+ *
+ * @author Javier Rodriguez Perez - 24435270R
+ */
 public class Coordinate {
-
+	
+	/**
+	 * x component
+	 */
+	private int x;
+	/**
+	 * y component
+	 */
+	private int y;
+	
+	/**
+	 * Creates a new coordinate with the given values
+	 * 
+	 * @param x The x value of the coordinate
+	 * @param y The y value of the coordinate
+	 */
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Creates a copy of the given coordinate
+	 * 
+	 * @param coordinate The coordinate to copy
+	 */
+	public Coordinate(Coordinate coordinate) {
+		this(coordinate.getX(), coordinate.getY());
+	}
+	
+	/**
+	 * Compares this coordinate with the given object
+	 * 
+	 * @param other The other object to compare
+	 * 
+	 * @return True if {@code this == other} or both components of the coordinates match, false otherwise 
+	 */
+	@Override
+	public boolean equals(Object other) {
+		if(this == other)
+			return true;
+		if(other == null)
+			return false;
+		if(!(other instanceof Coordinate))
+			return false;
+		Coordinate coordinate = (Coordinate) other;
+		return getX() == coordinate.getX() && getY() == coordinate.getY();
+	}
+	
+	/**
+	 * Creates a new coordinate with its components being the sum of the instance and the given coordinate
+	 * 
+	 * @param c The other coordinate, cannot be null
+	 * 
+	 * @return A new coordinate with its components being the sum of both coordinates
+	 */
+	public Coordinate add(Coordinate c) {
+		return add(c.getX(), c.getY());
+	}
+	
+	/**
+	 * Creates a new coordinate with its components being the sum of the instance and the given coordinate
+	 * 
+	 * @param x The x component
+	 * @param y The y component
+	 * 
+	 * @return A new coordinate with its components being the sum of both coordinates
+	 */
+	public Coordinate add(int x, int y) {
+		return new Coordinate(getX() + x, getY() + y);
+	}
+	
+	/**
+	 * @return The x component of the coordinate
+	 */
+	public int getX() {
+		return x;
+	}
+	
+	/**
+	 * @return The y component of the coordinate
+	 */
+	public int getY() {
+		return y;
+	}
+	
+	/**
+	 * @return A string representing the coordinate with the format {@code [x, y]}
+	 */
+	@Override
+	public String toString() {
+		return "[" + getX() + ", " + getY() + "]";
+	}
+	
 }
